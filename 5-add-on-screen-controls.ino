653143e2a967c6ddd66225e6ee0483a73816368a
Victoria Rönnlid <ronnlid@student.chalmers.se>
2024-04-16 16:11:08 +0200

    #5 Replace triangles with lines in drawUI method

--- 5-add-on-screen-controls.ino
+++ 5-add-on-screen-controls.ino
@@ -16,8 +16,9 @@ const int CIRCLE_RADIUS = 45;
 const int CENTER_X = 160;           // Middle point of screen X-axis
 const int CENTER_Y = 120;           // Middle point of screen Y-axis
 
-const int ARROW_TOP_OFFSET = 100;   // Distance from middle to the top of the arrow triangles
-const int ARROW_BASE_OFFSET = 60;   // Distance from middle to bottom sides of arrow triangles
+const int ARROW_TOP_OFFSET = 100;   // Distance from middle to the top of the arrows
+const int ARROW_BASE_OFFSET = 60;   // Distance from middle to bottom sides of arrows
+const int ARROW_LENGTH = ARROW_TOP_OFFSET - ARROW_BASE_OFFSET; // Value of arrow length
 const int ARROW_COLOR = TFT_WHITE;
 
 const int BACKGROUND_COLOR = TFT_BLACK;
@@ -32,20 +33,32 @@ const int okButton = WIO_5S_PRESS;
 void drawUI() {  // Method to draw the UI on screen
 
     tft.fillScreen(BACKGROUND_COLOR);   // Drawing black background
+    
     tft.drawCircle(CENTER_X, CENTER_Y, CIRCLE_RADIUS, CIRCLE_COLOR); // Draw middle circle
+    tft.fillCircle(CIRCLE_COLOR);
 
-    // Draw triangles to represent arrows
-    tft.drawTriangle(CENTER_X, CENTER_Y - ARROW_TOP_OFFSET, CENTER_X + ARROW_BASE_OFFSET, CENTER_Y, CENTER_X - ARROW_BASE_OFFSET, CENTER_Y, ARROW_COLOR);   // Top arrow
-    tft.drawTriangle(CENTER_X + ARROW_TOP_OFFSET, CENTER_Y, CENTER_X, CENTER_Y - ARROW_BASE_OFFSET, CENTER_X, CENTER_Y + ARROW_BASE_OFFSET, ARROW_COLOR);   // Right arrow
-    tft.drawTriangle(CENTER_X, CENTER_Y + ARROW_TOP_OFFSET, CENTER_X + ARROW_BASE_OFFSET, CENTER_Y, CENTER_X - ARROW_BASE_OFFSET, CENTER_Y, ARROW_COLOR);   // Down arrow
-    tft.drawTriangle(CENTER_X - ARROW_TOP_OFFSET, CENTER_Y, CENTER_X, CENTER_Y - ARROW_BASE_OFFSET, CENTER_X, CENTER_Y + ARROW_BASE_OFFSET, ARROW_COLOR);   // Left arrow
+    // Draw top arrow
+    tft.drawLine(CENTER_X, CENTER_Y - ARROW_TOP_OFFSET, CENTER_X + ARROW_LENGTH, CENTER_Y - ARROW_BASE_OFFSET, ARROW_COLOR);
+    tft.drawLine(CENTER_X, CENTER_Y - ARROW_TOP_OFFSET, CENTER_X - ARROW_LENGTH, CENTER_Y - ARROW_BASE_OFFSET, ARROW_COLOR);
+
+    // Draw right arrow
+    tft.drawLine(CENTER_X + ARROW_TOP_OFFSET, CENTER_Y, CENTER_X + ARROW_BASE_OFFSET, CENTER_Y + ARROW_LENGTH, ARROW_COLOR);
+    tft.drawLine(CENTER_X + ARROW_TOP_OFFSET, CENTER_Y, CENTER_X + ARROW_BASE_OFFSET, CENTER_Y - ARROW_LENGTH, ARROW_COLOR);
+
+    // Draw bottom arrow
+    tft.drawLine(CENTER_X, CENTER_Y + ARROW_TOP_OFFSET, CENTER_X - ARROW_LENGTH, CENTER_Y + ARROW_BASE_OFFSET, ARROW_COLOR);
+    tft.drawLine(CENTER_X, CENTER_Y + ARROW_TOP_OFFSET, CENTER_X + ARROW_LENGTH, CENTER_Y + ARROW_BASE_OFFSET, ARROW_COLOR);
+    
+    // Draw left arrow
+    tft.drawLine(CENTER_X - ARROW_TOP_OFFSET, CENTER_Y, CENTER_X - ARROW_BASE_OFFSET, CENTER_Y - ARROW_LENGTH, ARROW_COLOR);
+    tft.drawLine(CENTER_X - ARROW_TOP_OFFSET, CENTER_Y, CENTER_X - ARROW_BASE_OFFSET, CENTER_Y + ARROW_LENGTH, ARROW_COLOR);
 }
 
 void setup() {
 
     Serial.begin(9600);
     tft.begin();
-    tft.setRotation(1);
+    tft.setRotation(0);
 
     // Initialize button pins
     pinMode(upButton, INPUT); 
