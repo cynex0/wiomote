0d94a7986e49a2c46d12571895d29811fc45a8a7
Maksims Orlovs <orlovs@student.chalmers.se>
2024-04-16 13:57:48 +0200

    #8 Add button mapping

--- wiomote.ino
+++ wiomote.ino
@@ -1,3 +1,8 @@
+#include <string.h>
+
+#include <Dictionary.h>
+#include <DictionaryDeclarations.h>
+
 #include <IRLibSendBase.h>
 #include <IRLib_HashRaw.h>
 #include <IRLibCombo.h>
@@ -16,11 +21,16 @@ const int Y = 120;
 const int RADIUS = 50;
 const int COLOR = TFT_BLUE;
 
-uint16_t *rawData = nullptr;
-uint8_t dataLength = 0;
 bool receiveMode = false;
 
+Dictionary &commandMap = *(new Dictionary(6)); // char* -> char* map, maps buttons to commands
+
 int MoPin = D0;
+
+struct Command {
+  uint16_t *rawData;
+  uint8_t dataLength;
+};
 
 void resetUI(){
 	// Draw action button
@@ -38,12 +48,24 @@ void setup(){
 	Serial.begin(9600); // Start serial
 
 	// Set up input
-	pinMode(WIO_5S_PRESS, INPUT);
-	pinMode(WIO_KEY_A, INPUT);
-	pinMode(WIO_KEY_B, INPUT);
-	pinMode(WIO_KEY_C, INPUT);
+	pinMode(WIO_5S_PRESS, INPUT_PULLUP);
+	pinMode(WIO_5S_UP, INPUT_PULLUP);
+  pinMode(WIO_5S_DOWN, INPUT_PULLUP);
+  pinMode(WIO_5S_LEFT, INPUT_PULLUP);
+  pinMode(WIO_5S_RIGHT, INPUT_PULLUP);
+	pinMode(WIO_KEY_A, INPUT_PULLUP);
+	pinMode(WIO_KEY_B, INPUT_PULLUP);
+	pinMode(WIO_KEY_C, INPUT_PULLUP);
 
 	pinMode( MoPin, OUTPUT );
+
+  // set up buttons-commands map
+  commandMap("PRESS", nullptr);
+  commandMap("UP", nullptr);
+  commandMap("DOWN", nullptr);
+  commandMap("LEFT", nullptr);
+  commandMap("RIGHT", nullptr);
+  commandMap("POWER", nullptr);
 
 	tft.begin();							 // LCD initialization
 	tft.setRotation(3);						 // Setting LCD rotation
@@ -52,7 +74,61 @@ void setup(){
 	resetUI();
 }
 
-void sendData(uint16_t *data){
+char* buttonPressed(){
+  char *out = nullptr;
+  if (digitalRead(WIO_5S_PRESS) == LOW) out = "PRESS";
+  else if (digitalRead(WIO_5S_UP) == LOW) out = "UP";
+  else if (digitalRead(WIO_5S_DOWN) == LOW) out = "DOWN";
+  else if (digitalRead(WIO_5S_LEFT) == LOW) out = "LEFT";
+  else if (digitalRead(WIO_5S_RIGHT) == LOW) out = "RIGHT";
+  else if (digitalRead(WIO_KEY_A) == LOW) out = "POWER";
+  return out;
+}
+
+String serializeCommand(const Command& cmd){
+  // Format : "len, timing_1, timing_2, ..., timing_len"
+  String serialized = "";
+  serialized += String(cmd.dataLength) + ","; // Append data length to the string
+  
+  // Append each timing from rawData array to the string
+  for (int i = 0; i < cmd.dataLength; i++) {
+    serialized += String(cmd.rawData[i]);
+    if (i < cmd.dataLength - 1) {
+      serialized += ",";
+    }
+  }
+  
+  return serialized;
+}
+
+Command deserializeCommand(const String& serialized) {
+  // Format : "len, timing_1, timing_2, ..., timing_len"
+  Command cmd;
+  if (serialized == nullptr) {
+    cmd = {nullptr, 0}; // if null string received, return null command
+    return cmd;
+  } 
+  
+  int commaIndex = serialized.indexOf(',');  // get index of the first comma
+  cmd.dataLength = serialized.substring(0, commaIndex).toInt(); // get data length from the serialized string
+  
+  cmd.rawData = new uint16_t[cmd.dataLength]; // allocate memory for rawData array
+  commaIndex++; // move the index past the comma
+
+  // Extract each timing of rawData array from the string
+  for (uint8_t i = 0; i < cmd.dataLength; i++) {
+    int nextCommaIndex = serialized.indexOf(',', commaIndex);
+    if (nextCommaIndex == -1) {
+      nextCommaIndex = serialized.length();
+    }
+    cmd.rawData[i] = serialized.substring(commaIndex, nextCommaIndex).toInt();
+    commaIndex = nextCommaIndex + 1;
+  }
+  
+  return cmd;
+}
+
+void sendData(uint16_t *data, uint8_t dataLength){
 	if (data != nullptr){
 		emitter.send(data, dataLength, CARRIER_FREQUENCY_KHZ); // Pass the array, array length, carrier frequency
 
@@ -75,17 +151,28 @@ void switchToReceive(){
 	spr.pushSprite(0, 0);
 }
 
+
 void receive(){
 	receiver.enableIRIn();
 
 	if (receiver.getResults()){
-		dataLength = recvGlobal.recvLength;
-		rawData = new uint16_t[dataLength];
+		uint8_t dataLength = recvGlobal.recvLength;
+		uint16_t *rawData = new uint16_t[dataLength];
 
-		for (bufIndex_t i = 1; i < dataLength; i++){
+		for (uint8_t i = 1; i < dataLength; i++){
 			rawData[i - 1] = recvGlobal.recvBuffer[i];
 		}
 		rawData[dataLength - 1] = 1000; // Arbitrary trailing space
+    Command recCommand = {rawData, dataLength};
+    
+    // Save the signal to a button
+    spr.drawString("Received. Press button to save.", 10, 225);
+    spr.pushSprite(0, 0);
+    char *chosenButton;
+    do {
+      chosenButton = buttonPressed();
+    } while(chosenButton == nullptr); // wait for a button press
+    commandMap(chosenButton, serializeCommand(recCommand)); // write the received command to the map
 
 		resetUI();
 		receiver.disableIRIn();
@@ -98,21 +185,20 @@ void loop(){
 	if (receiveMode){
 		receive();
 	} else { // Detecting button presses
-		if (digitalRead(WIO_5S_PRESS) == LOW){
-			sendData(rawData);
+    if (digitalRead(WIO_KEY_C) == LOW){
+			switchToReceive(); // Enter receive mode
+		}
 
-			if (rawData != nullptr){ // Vibrate if data is present
-				digitalWrite(MoPin, HIGH);
+    char* pressed = buttonPressed();
+    if (pressed != nullptr) {
+      Command command = deserializeCommand(commandMap[pressed]);
+
+			if (command.rawData != nullptr){
+        sendData(command.rawData, command.dataLength);
+				digitalWrite(MoPin, HIGH); // vibrate if data sent
 				delay(250);
 				digitalWrite(MoPin, LOW);
 			}
-		}
-
-		if (digitalRead(WIO_KEY_A) == LOW ||
-			digitalRead(WIO_KEY_B) == LOW ||
-			digitalRead(WIO_KEY_C) == LOW){
-
-			switchToReceive(); // Enter receive mode
-		}
+    }		
 	}
 }
