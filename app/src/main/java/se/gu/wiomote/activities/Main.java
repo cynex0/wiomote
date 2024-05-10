a579037fb75218d14f32a4573f89b2552ebbee87
Victoria Rönnlid <ronnlid@student.chalmers.se>
2024-05-10 13:14:14 +0200

    #33 Add activity switching logic

--- app/src/main/java/se/gu/wiomote/activities/Main.java
+++ app/src/main/java/se/gu/wiomote/activities/Main.java
@@ -6,21 +6,60 @@ import android.os.Bundle;
 import se.gu.wiomote.R;
 import se.gu.wiomote.activities.remote.Remote;
 
-public class Main extends NotificationTrayActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+private static final int CONNECTION_TIMEOUT = 5000;
+private Handler handler = new Handler();
+private TextView messageTextView;
+private boolean connectionEstablished = false;
 
-        setContentView(R.layout.main);
+@Override
+protected void onCreate(Bundle savedInstanceState) {
 
-        findViewById(R.id.open).setOnClickListener(v -> {
-            Intent intent = new Intent(Main.this, Setup.class);
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
 
-            startActivity(intent);
-        });
+    statusTextView = findViewById(R.id.status_text_view);
+    statusTextView.setText("Looking for terminal connection...");
 
-        findViewById(R.id.remote).setOnClickListener(v -> {
-            startActivity(new Intent(Main.this, Remote.class));
-        });
+    handler.postDelayed(this::checkConnection, CONNECTION_TIMEOUT);
+
+}
+
+private void switchToActivity() {
+
+    if (connectionEstablished) {
+
+        startActivity(new Intent(Main.this, Remote.class));
+
+    } else {
+
+        startActivity(new Intent(Main.this, Setup.class));
+    }
+}
+
+private void checkConnection() {
+
+    WioMQTTClient.setOnConnectionStatusChangedListener(new WioMQTTClient.OnConnectionStatusChanged() {
+
+        @Override
+        public void onConnected() {
+
+            notificationContainer.setVisibility(View.GONE);
+            switchToActivity(Remote.class);
+            connectionEstablished(true);
+        }
+
+        @Override
+        public void onDisconnected() {
+
+            notification.setText(R.string.connection_lost);
+            notificationContainer.setVisibility(View.VISIBLE);
+            connectionEstablished(false);
+            switchToActivity(Setup.class);
+        }
+    }
+
+    protected void onDestroy(){
+        super.onDestroy();
+        handler.removeCallbacks(null);
     }
 }
